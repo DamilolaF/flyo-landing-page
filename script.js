ac6480ed2e8ace3c8fc6698f948f9c3d98038318
Damilola <dami_fagbo@yahoo.com>
2021-02-17 11:10:36 +0100

    added simple js

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.btn-primary');
+const mailInput = document.querySelector('.input-mail');
+
+button.addEventListener('click', function(){
+if (mailInput.validity.valid == false){
+alert('error, kindly input email');
+
+}
+});
